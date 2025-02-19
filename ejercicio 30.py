9f03d47495515072a23780dc6e9e1107fa34a319
Andrés Suárez <andresuarez1906@gmail.com>
2025-02-19 18:03:32 +0100

    Add files via upload

---
+++ ejercicio 30.py
@@ -0,0 +1,6 @@
+Crea una lista de números.
+Define una función recursiva suma_lista(lista) que:
+    Si la lista está vacía, retorne 0.
+    Si no está vacía, retorne el primer elemento más el resultado de suma_lista con el resto de la lista.
+Imprime el resultado de la suma al llamar la función.
+
